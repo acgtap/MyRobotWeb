379f5bba5cb9a9adf844d3f6f854d6273f36745f
acgtap <2480321637@qq.com>
2019-04-30 20:35:23 +0800

    换首页了

--- js/main.js
+++ js/main.js
@@ -1,12 +1,67 @@
-jQuery(document).ready(function($) {
+// *************************************************************************//
+// ! This is main JS file that contains custom scripts used in this template*/
+// *************************************************************************//
+/**
+	Navigation File
 
-	'use strict';
+	01. Carousel
+	02. Custom Select
+	03. Mobile Menu
 
-	/************** Mixitup (Filter Projects) *********************/
-    	$('.projects-holder').mixitup({
-            effects: ['fade','grayscale'],
-            easing: 'snap',
-            transitionSpeed: 400
-        });
+ */
+
+$( document ).ready(function() {
+	"use strict";
+	// **********************************************************************//
+	// 01. Carousel
+	// **********************************************************************//
+	$('.base-slider, .slider').owlCarousel({
+		loop: true,
+		margin: 0,
+		nav: true,
+		navText: ["<img src='templates/vultr/assets/images/arrow-left.png'>","<img src='templates/vultr/assets/images/arrow-right.png'>"],
+		dots: false,
+		item: 1,
+		responsive:{
+			0:{
+				items:1
+			},
+			600:{
+				items:1
+			},
+			1000:{
+				items:1
+			}
+		}
+	});
+	$('.partner-slider').owlCarousel({
+		loop: true,
+		margin: 0,
+		nav: true,
+		autoplay: true,
+		dots: false,
+		item: 5,
+		responsive:{
+			0:{
+				items:2
+			},
+			600:{
+				items:3
+			},
+			1000:{
+				items:5
+			}
+		}
+	});
+
+
+	// **********************************************************************//
+	// 03. Mobile Menu
+	// **********************************************************************//
+	$('.mobile-menu-btn').on('click', function(){
+		$(this).toggleClass('active');
+		$('header').toggleClass('active');
+		$('body').toggleClass('mobile-menu-open');
+	});
 
 });
